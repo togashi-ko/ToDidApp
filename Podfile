1e80cdb093104a07cf5637cc22d834beb6f57da4
冨樫公一 <koichi-_-626@outlook.com>
2018-01-21 20:54:42 +0900

    途中経過の移行とリファクタリング

---
+++ Podfile
@@ -0,0 +1,22 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'ToDidApp' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+  # Pods for ToDidApp
+    pod 'Firebase/Core'
+
+  target 'ToDidAppTests' do
+    inherit! :search_paths
+    # Pods for testing]
+    pod 'Firebase/Core'
+  end
+
+  target 'ToDidAppUITests' do
+    inherit! :search_paths
+    # Pods for testing
+    pod 'Firebase/Core'
+  end
+
+end
---
+++ ToDidApp/View/View/SelectActionView.swift
@@ -0,0 +1,46 @@
+//
+//  SelectActionView.swift
+//  ToDidApp
+//
+//  Created by 冨樫公一 on 2017/12/03.
+//  Copyright © 2017年 冨樫公一. All rights reserved.
+//
+
+import UIKit
+
+protocol SelectActionViewDelegate {
+    func removeAction()
+    func addAction()
+}
+
+class SelectActionView: UIView {
+
+    @IBOutlet weak var removeButton: UIButton!
+    @IBOutlet weak var addButton: UIButton!
+    @IBOutlet weak var actionListTableView: UITableView!
+    var delegate: SelectActionViewDelegate?
+
+    override init(frame: CGRect){
+        super.init(frame: frame)
+        loadNib()
+    }
+    
+    required init(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)!
+        loadNib()
+    }
+    
+    func loadNib(){
+        let view = Bundle.main.loadNibNamed("SelectActionView", owner: self, options: nil)?.first as! UIView
+        view.frame = self.bounds
+        self.addSubview(view)
+    }
+    
+    @IBAction func tapRemoveActionButton(_ sender: UIButton) {
+        self.delegate?.removeAction()
+    }
+
+    @IBAction func tapAddActionButton(_ sender: UIButton) {
+        self.delegate?.addAction()
+    }
+}
